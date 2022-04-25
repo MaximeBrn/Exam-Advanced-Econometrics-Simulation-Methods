2ffa7e2841c3ebcf78f6c8414a47cb6460d7c9bc
MaximeBrn <37322244+MaximeBrn@users.noreply.github.com>
2022-04-25 15:30:26 +0200

    Original xgrid function

--- Code Original Arcidiacono Miller 2011/CODE R/Files/xgrid.R
+++ Code Original Arcidiacono Miller 2011/CODE R/Files/xgrid.R
@@ -1,32 +1,17 @@
-# xgrid=function(theta,xval){
-# 
-#   n=length(xval)
-#   xub=c(xval[2:n],Inf) #
-#   xtran=matrix(0,n,n)
-#   xtranc=matrix(0,n,n)
-#   lcdf=0
-#   for(i in 1:n){
-#     xtran[,i]=(xub[i]>=xval)*(1-exp(-theta*(xub[i]-xval))-lcdf) # see eq. (7.2)
-#     lcdf=xtran[,i]+lcdf
-#     xtranc[,i]=xtranc[,i]+lcdf # cumulated distribution function
-#   }
-# 
-#   list(xtran=xtran,xtranc=xtranc)
-# }
-
 xgrid=function(theta,xval){
-  
+
   n=length(xval)
   xub=c(xval[2:n],Inf) #
   xtran=matrix(0,n,n)
   xtranc=matrix(0,n,n)
   lcdf=0
   for(i in 1:n){
-    xtran[,i]=(xub[i]>=xval)*ptruncnorm(xub[i]-xval,a=0,b=Inf,mean=2.7,sd=1.41)-lcdf
+    xtran[,i]=(xub[i]>=xval)*(1-exp(-theta*(xub[i]-xval))-lcdf) # see eq. (7.2)
     lcdf=xtran[,i]+lcdf
     xtranc[,i]=xtranc[,i]+lcdf # cumulated distribution function
   }
-  
+
   list(xtran=xtran,xtranc=xtranc)
 }
+
 
